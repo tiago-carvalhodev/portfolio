2b8db755f0a9e4ebaa49ac6fd5e7997136623344
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-11-28 17:30:23 +0100

    Meta image

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -11,13 +11,12 @@ const Portfolio = () => {
         <div data-project>
           <h3> Portfolio </h3>
           <img src={icon} width="300" height="150" alt="Screen from the project" />
-          <p> Project developed in the personal scope developed in React. </p>
-          <a data-btnS> Check it out </a>
+          <p> Project developed on the personal scope developed in React. </p>
         </div>
         <div data-project>
           <h3> Le Koala Autrement </h3>
           <img src={lka} width="300" height="150" alt="Screen from the project" />
-          <p> Project developed in a professional scope for Koalità company developed in Jamstack using GatsbyJS, a React framework for the frontend and Sanity.io as CMS. </p>
+          <p> Project developed on a professional scope for Koalità company developed in Jamstack using GatsbyJS, a React framework for the frontend and Sanity.io as CMS. </p>
           <a href="https://lekoalaautrement.corsica" target="_blank" data-btnS> Check it out </a>
         </div>
       </div>
