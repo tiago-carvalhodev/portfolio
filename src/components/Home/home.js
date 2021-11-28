127fd65a45d5a0f50ee06b7dacaab087dbb6186b
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-11-28 18:04:18 +0100

    Data update

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -14,12 +14,9 @@ class Home extends Component {
           <div data-f>
             <h1> Hello there, welcome to my Portfolio </h1>
             <div data-f2 >
-              <p> Tiago Carvalho 21 years old , Portuguese currently living in Ajaccio, France,
-    Studying for a diplome Bachelor's degree in Conception of web and mobile applications.
-    Currently Junior Dev at koalità.
-    Tiago Carvalho 21 years old , Portuguese currently living in Ajaccio, France,
-    Studying for a diplome Bachelor's degree in Conception of web and mobile applications.
-    Currently junior Web developer at koalità. </p>
+              <p> Tiago Carvalho 21 years old , Portuguese currently living in Ajaccio, France. </p>
+              <p> Studying for a diplome Bachelor's degree in Conception of web and mobile applications.
+                  Currently Junior Dev at koalità.</p>
             </div>
             <a href="/work" data-btn> KNOW MORE </a>
           </div>
